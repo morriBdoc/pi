6b2730a673d49de8cc7264de419f3ec7a768898c
Morne Buitendag <morne.buitendag.personal@gmail.com>
2023-09-25 13:56:00 +0200

    Update pi.py

--- pi.py
+++ pi.py
@@ -2,12 +2,15 @@ from mpmath import mp
 
 decimals = input('How many decimal places: ')
 
+# You can change this if you have a good processor
+MAX_decimals = 100000 # DEFAULT: 100000
+
 if decimals.isdigit() == False:
     print('The input is not a valid digit.')
     input()
 else:
     mp.dps = int(decimals) + 1
-    if mp.dps - 1 > 100000:
+    if mp.dps - 1 > MAX_decimals:
         print('Only 100000 decimal digits can be printed.')
         input()
     else:
